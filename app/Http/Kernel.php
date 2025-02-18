43c32a07d86be79faaf6d50b3021ad98bcbfd971
sanchirEs <sanchirenkhamgalan@gmail.com>
2025-02-18 20:47:41 +0800

    kernel update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -9,16 +9,48 @@ class Kernel extends HttpKernel
     /**
      * The application's global HTTP middleware stack.
      *
+     * These middleware are run during every request to your application.
+     *
      * @var array
      */
     protected $middleware = [
-        \App\Http\Middleware\TrustProxies::class, // <-- Add this
-        \Fruitcake\Cors\HandleCors::class,
+        \App\Http\Middleware\TrustProxies::class, // Trust proxies globally
+        \Fruitcake\Cors\HandleCors::class, // if you're using Cors
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
-    // ...
+    /**
+     * The application's route middleware groups.
+     *
+     * @var array
+     */
+    protected $middlewareGroups = [
+        'web' => [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            // If needed: \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+
+        'api' => [
+            // API middleware, if you use any...
+            'throttle:api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+    ];
+
+    /**
+     * The application's route middleware.
+     *
+     * @var array
+     */
+    protected $routeMiddleware = [
+        // Your route middleware here...
+    ];
 }
