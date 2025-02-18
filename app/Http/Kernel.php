e226e282eeca523b63734dd93f7543a4db8e2edb
sanchirEs <sanchirenkhamgalan@gmail.com>
2025-02-18 21:08:29 +0800

    bolchoochde

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,10 +29,10 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
+            \App\Http\Middleware\TrustProxies::class, // make sure this is here
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
-            // If needed: \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
