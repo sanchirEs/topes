a843e24530abb1189039129394558707fb6408bf
lkhagvaa <lhagbaa876@gmail.com>
2025-03-12 20:36:07 +0800

    a

--- config/database.php
+++ config/database.php
@@ -58,6 +58,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::ATTR_TIMEOUT => 30,
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
