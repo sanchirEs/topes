248b2e9a8787d0e8283c47a45621c026cf12e9f5
sanchirEs <sanchirenkhamgalan@gmail.com>
2025-05-18 13:57:24 +0800

    admin

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Filament\Facades\Filament;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductQuestionController;
+
+Filament::routes(); // ← this boots all the /admin routes
+
 
 View::composer(['layouts.master'],function ($view) {
   $view->with('categories', \App\Models\Product_category::orderBy('sort_order','ASC')->get());
@@ -18,7 +22,7 @@ Route::get('/shop', [HomeController::class, 'shop'])->name('shopdetail');
 Route::get('/shopcategory/{id}', [HomeController::class, 'shopcategory'])->name('shopcategory');
 Route::get('/{categorylink}/product/{id}', [HomeController::class, 'product'])->name('product');
 Route::get('/contact', [HomeController::class, 'contact'])->name('contactpage');
-Route::post('/upload-file', [FileUploadController::class, 'upload']);
+// Route::post('/upload-file', [FileUploadController::class, 'upload']);
 Route::post('/questions/store', [ProductQuestionController::class, 'store'])->name('questions.store');
 Route::post('/questions/{id}/reply', [ProductQuestionController::class, 'reply'])->name('questions.reply');
 Route::delete('/questions/{id}', [ProductQuestionController::class, 'destroy'])->name('questions.destroy');
